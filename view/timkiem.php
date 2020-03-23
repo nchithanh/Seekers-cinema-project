0eb67effadf4b9f139ccbc9f4fbaa581d53972f0
nchithanh <nchithanh9999@gmail.com>
2020-03-23 13:05:48 +0700

    up layout trang tim kiem

---
+++ view/timkiem.php
@@ -0,0 +1,60 @@
+
+        <div class="trangtimkiem">
+            <div class="khungketqua">
+                <div class="ketqua">
+                    <div class="hinhphim">
+                        <img src="img/anhphim1.jpg" alt="">
+                        <div class="chitiet">
+                            <a href="">chi tiet</a>
+                        </div>
+                    </div>
+                    <div class="tenphim">
+                        aaaaaaaaaaaaaaaaaaaaa
+                    </div>
+                </div>
+                <div class="ketqua">
+                    <div class="hinhphim">
+                        <img src="img/anhphim12.jpg" alt="">
+                        <div class="chitiet">
+                            <a href="">chi tiet</a>
+                        </div>
+                    </div>
+                    <div class="tenphim">
+                        aaaaaaaaaaaaaaaaaaaaa
+                    </div>
+                </div>
+                <div class="ketqua">
+                    <div class="hinhphim">
+                        <img src="img/anhphim12.jpg" alt="">
+                        <div class="chitiet">
+                            <a href="">chi tiet</a>
+                        </div>
+                    </div>
+                    <div class="tenphim">
+                        aaaaaaaaaaaaaaaaaaaaa
+                    </div>
+                </div>
+                <div class="ketqua">
+                    <div class="hinhphim">
+                        <img src="img/anhphim12.jpg" alt="">
+                        <div class="chitiet">
+                            <a href="">chi tiet</a>
+                        </div>
+                    </div>
+                    <div class="tenphim">
+                        aaaaaaaaaaaaaaaaaaaaa
+                    </div>
+                </div>
+                <div class="ketqua">
+                    <div class="hinhphim">
+                        <img src="img/anhphim12.jpg" alt="">
+                        <div class="chitiet">
+                            <a href="">chi tiet</a>
+                        </div>
+                    </div>
+                    <div class="tenphim">
+                        aaaaaaaaaaaaaaaaaaaaa
+                    </div>
+                </div>
+            </div>
+        </div>
