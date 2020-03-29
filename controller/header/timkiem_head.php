c1a8713aed2082755a9629bca5448d4e822fc61d
dangson99 <sondhpk01312@fpt.edu.vn>
2020-03-29 20:05:34 +0700

    fix loi tim kiem

--- controller/header/timkiem_head.php
+++ controller/header/timkiem_head.php
@@ -2,7 +2,8 @@
  include "model/connect.php";
  include_once "model/timkiem_sql.php";
  if(isset($_POST['tim'])&&$_POST['tim']){
-     $noidung=$_POST['noidung'];
+     
+     $noidung=htmlspecialchars($_POST['noidung'],ENT_QUOTES);
     $timkiem=timkiem($noidung);
  }
 
