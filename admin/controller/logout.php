88e6091694172791864dc808aa784e46249aa5be
maggicnapol <maggicnapol@gmail.com>
2020-04-04 10:28:53 +0700

    rm
    Sua admin

--- admin/controller/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "model/connect.php";
-include_once "model/user.php";
- unset($_SESSION['id']);
- unset($_SESSION['user']);
- unset($_SESSION['role']);
- header('location: ../index.php') ;
-?>
--- admin/controller/quanliphim/phimql_ql.php
+++
@@ -1,7 +0,0 @@
-<?php
- include "model/connect.php";
- include_once "model/phim_sql.php";
- $quanliphimdc=quanliphim(1);
- $quanliphimcc=quanliphim(0);
- 
-?>
